37a19abddd67380826eb6d1070d6af3228617505
ProDataMan <antoine.victor@prodataman.com>
2025-04-18 12:44:48 -0700

    changed Navebar to navbar

--- VoltCafeUI/src/App.jsx
+++ VoltCafeUI/src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar.jsx'
 import Menu from './pages/Menu';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
